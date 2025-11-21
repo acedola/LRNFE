1c6e7c44599b2cf9ccc223a4de0eecdac37c6117
root <root@DESKTOP-TJ8U1HO.localdomain>
2025-11-21 18:20:04 -0300

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Opciones comunes:
+  reactStrictMode: true,
+  images: {
+    domains: ['ejemplo.com'],
+  },
+  // Otras configuraciones que necesites
+};
+
+module.exports = nextConfig;
